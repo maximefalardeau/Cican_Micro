7eddfcb7d4a76f80bd7d4da2b008928c6b66d290
Maxime Falardeau <maxime.falardeau@gmail.com>
2018-09-26 22:46:16 -0400

    Modifcation des DiplayName, Déplacement du .docx

--- Cican_Micro/Models/Produits.cs
+++ Cican_Micro/Models/Produits.cs
@@ -8,14 +8,16 @@ namespace Cican_Micro.Models
 {
     public class Produits
     {
-            public int ID { get; set; }
-            [Display(Name = "Image")]
-            public string ImageUrl { get; set; }
-            public string Nom { get; set; }
-            public string Modele { get; set; }
-            public string Categorie { get; set; }
-            [DataType(DataType.Currency)]
-            public decimal prix { get; set; }
-        
+        public int ID { get; set; }
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+        public string Nom { get; set; }
+        [Display(Name = "Modèle")]
+        public string Modele { get; set; }
+        [Display(Name = "Catégorie")]
+        public string Categorie { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Prix { get; set; }
+
     }
 }
